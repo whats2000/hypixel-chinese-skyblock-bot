dc658b073ec06c59d75d932c8f85af3bc5cefbc5
whats2000 <60466660+whats2000@users.noreply.github.com>
2021-11-15 01:36:50 +0800

    Add slash command for embed builder

---
+++ hypixel_chinese_skyblock_bot/SlashCommands/SlashEmbed.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+from dislash import slash_command, OptionType
+from dislash.slash_commands import Option
+
+from hypixel_chinese_skyblock_bot.Core.Common import CodExtension, get_setting_json
+
+
+class SlashEmbed(CodExtension):
+    @slash_command(
+        guild_ids=[int(get_setting_json('ServerId'))],
+        name="embed",
+        description="build a embed",
+        options=[
+            Option('title', 'Makes the title of the embed', OptionType.STRING),
+            Option('description', 'Makes the description', OptionType.STRING),
+            Option('color', 'The color of the embed', OptionType.STRING)
+        ]
+    )
+    async def embed(self, inter, title=None, description=None, color=None):
+        if color is not None:
+            try:
+                color = await commands.ColorConverter().convert(inter, color)
+            except:
+                color = None
+
+        if color is None:
+            color = discord.Color.default()
+
+        emb = discord.Embed(color=color)
+
+        if title is not None:
+            emb.title = title
+
+        if description is not None:
+            emb.description = description
+
+        await inter.respond(embed=emb)
+
+
+def setup(pybot):
+    pybot.add_cog(SlashEmbed(pybot))
