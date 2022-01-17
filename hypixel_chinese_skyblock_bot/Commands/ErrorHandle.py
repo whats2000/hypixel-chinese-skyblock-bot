992d65d6be37bd73452bb37ea565d14670ca39fe
whats2000 <60466660+whats2000@users.noreply.github.com>
2022-01-17 13:45:57 +0800

    Add error listener

---
+++ hypixel_chinese_skyblock_bot/Commands/ErrorHandle.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+
+from hypixel_chinese_skyblock_bot.Core.Common import CodExtension
+
+
+class ErrorHandler(CodExtension):
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError):
+        if isinstance(error, commands.CommandNotFound):
+            message = '未知指令!'
+
+        elif isinstance(error, commands.CommandOnCooldown):
+            message = f'此指令在冷卻中。 請在 {round(error.retry_after, 1)} 秒後重試'
+
+        elif isinstance(error, commands.MissingPermissions):
+            message = '你沒有權限使用該指令!'
+
+        elif isinstance(error, commands.UserInputError):
+            message = '你的輸入內容有誤, 請檢查你的輸入內容!'
+
+        else:
+            message = '運行該命令時發生未知錯誤!'
+
+        print('> 出現錯誤 : ' + message)
+
+        embed = discord.Embed(
+            title="錯誤狀況",
+            description=message,
+            color=0xe74c3c
+        )
+
+        embed.set_author(
+            name=ctx.message.author.name,
+            icon_url=ctx.message.author.avatar_url
+        )
+
+        await ctx.send(embed=embed, delete_after=20.0)
+
+        await ctx.message.delete()
+
+
+def setup(pybot):
+    pybot.add_cog(ErrorHandler(pybot))
