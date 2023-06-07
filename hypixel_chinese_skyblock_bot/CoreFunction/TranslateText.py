a1ceb6288abd2f198fd5c1cace711f265f899818
whats2000 <whats2000mc@gmail.com>
2023-06-07 22:39:46 +0800

    Add google translate of wiki

--- hypixel_chinese_skyblock_bot/CoreFunction/TranslateText.py
+++ hypixel_chinese_skyblock_bot/CoreFunction/TranslateText.py
@@ -1,6 +1,7 @@
 import logging
 
 import googletrans
+from googletrans.models import Translated
 
 from CoreFunction.Logger import Logger
 
@@ -10,20 +11,25 @@ bot_logger = Logger(__name__)
 def translate_text(originals: str, targets: str, txt: str):
     t = googletrans.Translator()
 
-    if originals is None:
+    try:
+        if originals is None:
 
-        result = t.translate(
-            txt,
-            dest=targets
-        )
+            result = t.translate(
+                txt,
+                dest=targets
+            )
 
-    else:
+        else:
+            result = t.translate(
+                txt,
+                src=originals,
+                dest=targets
+            )
+        bot_logger.log_message(logging.DEBUG, f'Translate > {txt} --> {result.text} ({targets})')
 
-        result = t.translate(
-            txt,
-            src=originals,
-            dest=targets
-        )
-    bot_logger.log_message(logging.INFO, f'Translate > {txt} --> {result.text} ({targets})')
+    except TypeError:
+        bot_logger.log_message(logging.ERROR, 'Translator Fail')
+
+        return Translated(src=None ,dest=targets, origin=originals, text=txt, parts=[], pronunciation=None)
 
     return result
--- hypixel_chinese_skyblock_bot/SlashCommands/SlashWiki.py
+++ hypixel_chinese_skyblock_bot/SlashCommands/SlashWiki.py
@@ -2,12 +2,35 @@ import disnake
 import requests
 from bs4 import BeautifulSoup
 from disnake.ext import commands
+from googletrans.constants import LANGCODES, LANGUAGES
 
 from CoreFunction.Common import CodExtension, get_setting_json
 from CoreFunction.Logger import Logger
 from CoreFunction.SendEmbed import set_inter_embed_author
+from CoreFunction.TranslateText import translate_text
 
 bot_logger = Logger(__name__)
+
+
+async def autocomplete_langs(inter: disnake.ApplicationCommandInteraction, user_input: str):
+    results = [lang for code, lang in zip(LANGCODES, LANGUAGES) if user_input.lower() in lang]
+    return results[:25]
+
+
+def get_translate(text: str = None, translate: str = None):
+    if not text or not text.strip():
+        return text
+
+    if not translate or not translate.strip():
+        return text
+
+    result = translate_text("en", translate, text)
+
+    if len(result.text) > 15:
+        return result.text + f'```\n{text}\n```'
+    else:
+        return result.text + f' [{text}]'
+
 
 
 class SlashWiki(CodExtension):
@@ -19,8 +42,24 @@ class SlashWiki(CodExtension):
     async def wiki(self, inter: disnake.AppCommandInteraction,
                    query: str = commands.Param(
                        description='The search input'
+                   ),
+                   translate: str = commands.Param(
+                       description='[!]Warning[!] this is using google translate will not 100% correct]',
+                       autocomplete=autocomplete_langs,
+                       default=None
                    )):
         await inter.response.defer()
+
+        if translate is not None and translate not in [lang for code, lang in zip(LANGCODES, LANGUAGES)]:
+            embed = disnake.Embed(
+                title='查詢失敗',
+                description='訪問 wiki 失敗，你輸入未知語言',
+                color=0xe74c3c
+            )
+
+            set_inter_embed_author(embed, inter)
+
+            await inter.send(embed=embed, ephemeral=True)
 
         url = f'https://wiki.hypixel.net/index.php?search={query}&title=Special%3ASearch'
 
@@ -34,7 +73,7 @@ class SlashWiki(CodExtension):
             result_title = soup.find('div', {'class': 'page-heading'}).text.strip()
 
             embed = disnake.Embed(
-                title=result_title,
+                title=get_translate(result_title, translate),
                 description=f'搜尋 {query} 結果',
                 url=final_url,
                 color=0x7289DA
@@ -50,10 +89,15 @@ class SlashWiki(CodExtension):
                         if name_links:
                             for name_link in name_links:
                                 link_title = name_link.text.strip()
+
                                 link_url = 'https://wiki.hypixel.net' + name_link['href']
 
                                 if link_title:
-                                    embed.add_field(name=link_title, value=link_url, inline=False)
+                                    embed.add_field(
+                                        name=get_translate(link_title, translate),
+                                        value=link_url,
+                                        inline=False
+                                    )
                                     break
             elif result_title == 'Search results':
                 search_results = soup.find('ul', {'class': 'mw-search-results'})
@@ -63,8 +107,10 @@ class SlashWiki(CodExtension):
 
                     for link in links:
                         link_title = link.text.strip()
+                        link_title += get_translate(link_title, translate)
+
                         link_url = 'https://wiki.hypixel.net' + link['href']
-                        embed.add_field(name=link_title, value=link_url, inline=False)
+                        embed.add_field(name=get_translate(link_title, translate), value=link_url, inline=False)
             else:
                 info_box = soup.find('table', {'class': 'infobox'})
 
@@ -90,25 +136,26 @@ class SlashWiki(CodExtension):
 
                             if field == 'Category':
                                 value = value_cell.get_text(strip=True)
-                                embed.add_field(name='分類: ', value=value, inline=False)
+                                embed.add_field(name='分類: ', value=get_translate(value, translate), inline=False)
 
                             elif field == 'Type':
                                 value = value_cell.get_text(strip=True)
 
                                 if value not in added_types:
-                                    embed.add_field(name='類型: ', value=value, inline=False)
+                                    embed.add_field(name='類型: ', value=get_translate(value, translate), inline=False)
 
                                     added_types.append(value)
 
                             elif field == 'Rarity':
                                 value = value_cell.get_text(strip=True)
-                                embed.add_field(name='稀有度: ', value=value, inline=False)
+                                embed.add_field(name='稀有度: ', value=get_translate(value, translate), inline=False)
 
                             elif field == 'Internal ID':
                                 value_div = value_cell.find('div', class_='hp-spoiler-text')
 
                                 if value_div:
-                                    value = value_div.decode_contents(formatter="html").replace("<br/>", "\n").replace("<wbr/>", "")
+                                    value = value_div.decode_contents(formatter="html").replace("<br/>", "\n").replace(
+                                        "<wbr/>", "")
                                 else:
                                     value = value_cell.text.strip()
 
@@ -132,11 +179,14 @@ class SlashWiki(CodExtension):
                                 description_text = next_element.text.strip()
 
                                 if description_text is not None:
-                                    embed.add_field(name=title, value=description_text, inline=False)
+                                    embed.add_field(
+                                        name=get_translate(title, translate),
+                                        value=get_translate(description_text, translate),
+                                        inline=False
+                                    )
                                     title = ' '
 
                             next_element = next_element.find_next_sibling()
-
 
             set_inter_embed_author(embed, inter)
 
@@ -151,6 +201,7 @@ class SlashWiki(CodExtension):
             set_inter_embed_author(embed, inter)
 
             await inter.send(embed=embed, ephemeral=True)
+
 
 def setup(pybot):
     pybot.add_cog(SlashWiki(pybot))
