86f5bd3b1fe09bf657d134ff6ee48e66a6ac30bc
whats2000 <60466660+whats2000@users.noreply.github.com>
2023-11-15 16:20:18 +0800

    Add ApplyRole.py

---
+++ hypixel_chinese_skyblock_bot/UserCommands/ApplyRole.py
@@ -0,0 +1,49 @@
+import disnake
+import logging
+
+from disnake.ext import commands
+
+from CoreFunction.Common import get_setting_json, CodExtension, add_role, remove_role
+from CoreFunction.Logger import Logger
+
+bot_logger = Logger(__name__)
+
+
+class RoleAssigner(CodExtension):
+    @commands.has_any_role(get_setting_json('AdminRole'))
+    @commands.user_command(name="讓 TA 冷靜一下")  # Assign role
+    async def assign_role(self, inter: disnake.ApplicationCommandInteraction, user: disnake.User):
+        bot_logger.log_message(logging.INFO, f'Add Calm Down role to User : {inter.channel.name} | {inter.author.name}')
+
+        role_to_assign_id = 1174176502217199687
+
+        # Find the role in the guild
+        role = inter.guild.get_role(role_to_assign_id)
+        if not role:
+            await inter.response.send_message(f"Role with ID `{role_to_assign_id}` not found.", ephemeral=True)
+            return
+
+        # Assign the role
+        await add_role(ctx=inter, role_id=role_to_assign_id)
+        await inter.response.send_message(f"Assigned role to {user.mention}.", ephemeral=True)
+
+    @commands.has_any_role(get_setting_json('AdminRole'))
+    @commands.user_command(name="讓 TA 自由")  # Remove role
+    async def remove_role_command(self, inter: disnake.ApplicationCommandInteraction, user: disnake.User):
+        bot_logger.log_message(logging.INFO, f'Remove Calm Down role from User : {inter.channel.name} | {inter.author.name}')
+
+        role_to_remove_id = 1174176502217199687
+
+        # Find the role in the guild
+        role = inter.guild.get_role(role_to_remove_id)
+        if not role:
+            await inter.response.send_message(f"Role with ID `{role_to_remove_id}` not found.", ephemeral=True)
+            return
+
+        # Remove the role
+        await remove_role(ctx=inter, role_id=role_to_remove_id)
+        await inter.response.send_message(f"Removed role from {user.mention}.", ephemeral=True)
+
+
+def setup(pybot):
+    pybot.add_cog(RoleAssigner(pybot))
--- hypixel_chinese_skyblock_bot/__main__.py
+++ hypixel_chinese_skyblock_bot/__main__.py
@@ -37,5 +37,13 @@ for filename in os.listdir('SlashCommands'):
 
     pybot.load_extension(f'SlashCommands.{filename[:-3]}')
 
+for filename in os.listdir('UserCommands'):
+    if not filename.endswith('.py'):
+        continue
+
+    bot_logger.log_message(logging.DEBUG, f'Setup > UserCommands.{filename[:-3]}')
+
+    pybot.load_extension(f'UserCommands.{filename[:-3]}')
+
 if __name__ == '__main__':
     pybot.run((get_setting_json('Token')))
